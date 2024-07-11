7395c1ecbcef96cce8d0c1a9960ef5b8fba804b8
spriveen <rpeanuka@gmail.com>
2024-07-11 02:32:38 -0700

    update app.js

--- riveenportfolio/src/pages/_app.js
+++ riveenportfolio/src/pages/_app.js
@@ -1,5 +1,19 @@
+
+import Navbar from "@/components/NavBar";
 import "@/styles/globals.css";
+import {Montserrat} from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont"
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${montserrat.variable} font-mont  bg-light w-full min-h-screen`}>
+  <Navbar/>
+  <Component {...pageProps} />
+  </main>
+  )
 }
+
