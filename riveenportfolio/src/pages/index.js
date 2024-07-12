efb48131747f1adebb198eb512b6164e37123484
spriveen <rpeanuka@gmail.com>
2024-07-12 09:55:06 -0700

    update animated.js

--- riveenportfolio/src/pages/index.js
+++ riveenportfolio/src/pages/index.js
@@ -1,17 +1,18 @@
 import Layout from "@/components/Layout";
 import Image from "next/image";
 import profilePic from "../../public/images/profile/developer-pic-1.png"
+import AnimatedText from "@/components/AnimatedText";
 
 export default function Home() {
   return (
    <main className='flex items-center text-dark w-full min-h-screen' >
     <Layout className='pt-0'>
-     <div className="flex.item-center.justify-between w-full">
+     <div className="flex item-center justify-between w-full">
          <div className='w-1/2'>
           <Image src={profilePic} alt="Riveen Peanuka" className='w-full  h-auto'/>
          </div>
          <div className="w-1/2">
-          <h1>Turning Vision Reality With Code And Design</h1>
+          <AnimatedText text="Turning Vision Reality With Code And Design."/>
           <p>
           As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
           Explore my latest projects and articles, showcasing my expertise in React.js and web development.
