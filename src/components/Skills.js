b747654f7c8d5df5fddb5a44f3024f3887ea1e6a
spriveen <rpeanuka@gmail.com>
2024-07-14 20:04:38 -0700

    update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,20 @@ import React from 'react'
 import { motion } from "framer-motion"
 
 const Skill = ({ name, x, y }) => {
-    // Your Skill component logic here
+    return(
+        <motion.div 
+        className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
+        py-3 px-6 shadow-dark cursor-pointer absolute
+        '
+        whileHover={{ scale: 1.05 }}
+        initial={{x:0, y:0}}
+        animate={{x:x, y:y}}
+        transition={{duration: 1.5}}
+      >
+        {name}
+    </motion.div>
+    )
+
 }
 
 const Skills = () => {
@@ -17,6 +30,17 @@ const Skills = () => {
                 >
                     Web
                 </motion.div>
+                <Skill name="HTML" x="-20vw" y="2vw"/>
+                <Skill name="CSS" x="-5vw" y=" -10vw"/>
+                <Skill name="Javascript" x="20vw" y="6vw"/>
+                <Skill name="ReactJs" x="0vw" y="12vw"/>
+                <Skill name="NextJs" x="-20vw" y="-15vw"/>
+                <Skill name="Gatsby" x="15vw" y="-12vw"/>
+                <Skill name="Web Design" x="32vw" y="-5vw"/>
+                <Skill name="Figma" x="0vw" y=" -20vw"/>
+                <Skill name="Firebase" x="-25vw" y=" 18vw"/>
+                <Skill name="Tailwind CSS" x="18vw" y="-18vw"/>
+
             </div>
         </div>
     )
