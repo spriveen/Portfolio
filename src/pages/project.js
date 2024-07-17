70c4f20d67f703728de3c9cd04d50f1ecda84921
spriveen <rpeanuka@gmail.com>
2024-07-17 08:26:17 -0700

    updte project.js

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,93 +1,146 @@
-import Layout from '@/components/Layout'
-import React from 'react'
-import Head from 'next/head'
-import AnimatedText from '@/components/AnimatedText'
-import Link from 'next/link'
-import Image from 'next/image'
-import { GithubIcon } from '@/components/Icon'
+import Layout from "@/components/Layout";
+import React from "react";
+import Head from "next/head";
+import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import Image from "next/image";
+import { GithubIcon } from "@/components/Icon";
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
 
- const FeaturedProject = ({type, title, img, link, github, summary}) =>{
-  return(
-    <article>
-      <Link href={link} target='_blank'>
-      <Image src={img} alt={title} className="w-full h-auto" />
+const FeaturedProject = ({ type, title, img, link, github, summary }) => {
+  return (
+    <article
+      className="w-full flex items-center justify-between
+    rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12
+    
+    "
+    >
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
       </Link>
-      <div>
-        <span>{type}</span>
-        <Link href={link} target='_blank'>
-        <h2>{title}</h2>
-      </Link>
-      <p>{summary}</p>
-      <div>
-      <Link href={github} target='_blank'> <GithubIcon /></Link>
-      <Link href={link} target='_blank'>Visit project</Link>
-      </div>
-      </div>
 
+      <div className="w-1/2 flex flex-col items-center justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
+            {" "}
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
+            Visit project
+          </Link>
+        </div>
+      </div>
     </article>
-  )
- }
+  );
+};
 
+const Project = ({ title, type, img, link, github }) => {
+  return (
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl
+    border border-solid border-dark bg-light p-6 relative
+    
+    ">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
 
-
-
-
-
+      <div className="w-full flex flex-col items-center justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
+            {" "}
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
+            Visit
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 const project = () => {
   return (
     <>
-    <Head>
+      <Head>
         <title>Riveen Peanuka | projects Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main className='w-full mb-16 flex flex-col items-center justify-center'>
-        <Layout className='pt-16'>
-            <AnimatedText  text="Imagination Trumps Knowledge!" />
-        
-        <div className='grid grid-cols-12 gap-24'>
-          <div className='col-span-12'>
-            < FeaturedProject 
-    title=" Crypto Screener Application"
-     summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Imagination Trumps Knowledge!"
+            className="mb-16"
+          />
+
+          <div className="grid grid-cols-12 gap-24">
+            <div className="col-span-12">
+              <FeaturedProject
+                title=" Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
 It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
 local currency."
-link="/"
-github="/"
-type="Featured Project"       
-            
-           
-
-            
-            
-           
-           
-           />
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">
+            <Project
+                title=" Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">Project-2</div>
+            <div className="col-span-12">Featured Project</div>
+            <div className="col-span-6">Project-3</div>
+            <div className="col-span-6">Project-4</div>
           </div>
-          <div className='col-span-6'>
-               Project-1
-          </div>
-          <div className='col-span-6'>
-               Project-2
-          </div>
-          <div className='col-span-12'>
-               Featured Project
-          </div>
-          <div className='col-span-6'>
-               Project-3
-          </div>
-          <div className='col-span-6'>
-               Project-4
-          </div>
-        </div>
-        
-        
-        
         </Layout>
       </main>
-    
     </>
-  )
-}
+  );
+};
 
 export default project;
