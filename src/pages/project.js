1fb7b9c839490132b5666146add5f42502494653
spriveen <rpeanuka@gmail.com>
2024-07-17 23:35:19 -0700

    update article. js

--- src/pages/project.js
+++ src/pages/project.js
@@ -10,11 +10,13 @@ import project1 from "../../public/images/projects/crypto-screener-cover-image.j
 const FeaturedProject = ({ type, title, img, link, github, summary }) => {
   return (
     <article
-      className="w-full flex items-center justify-between
+      className="w-full flex items-center justify-between relative rounded-br-2xl
     rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12
-    
-    "
+     "
     >
+     <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark
+     rounded-br-3xl
+     '/> 
       <Link
         href={link}
         target="_blank"
@@ -55,8 +57,11 @@ const Project = ({ title, type, img, link, github }) => {
   return (
     <article className="w-full flex flex-col items-center justify-center rounded-2xl
     border border-solid border-dark bg-light p-6 relative
-    
     ">
+
+<div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
+     rounded-br-3xl
+     '/> 
       <Link
         href={link}
         target="_blank"
@@ -65,28 +70,28 @@ const Project = ({ title, type, img, link, github }) => {
         <Image src={img} alt={title} className="w-full h-auto" />
       </Link>
 
-      <div className="w-full flex flex-col items-center justify-between pl-6">
+      <div className="w-full flex flex-col items-center justify-between mt-4">
         <span className="text-primary font-medium text-xl">{type}</span>
         <Link
           href={link}
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
         </Link>
         
-        <div className="mt-2 flex items-center">
-          <Link href={github} target="_blank" className="w-10">
-            {" "}
-            <GithubIcon />
-          </Link>
-          <Link
+        <div className="w-full mt-2 flex items-center justify-between">
+        <Link
             href={link}
             target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+            className="text-lg font-semibold underline"
           >
             Visit
           </Link>
+          <Link href={github} target="_blank" className="w-10">
+            <GithubIcon />{""}
+          </Link>
+          
         </div>
       </div>
     </article>
@@ -107,7 +112,7 @@ const project = () => {
             className="mb-16"
           />
 
-          <div className="grid grid-cols-12 gap-24">
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
             <div className="col-span-12">
               <FeaturedProject
                 title=" Crypto Screener Application"
@@ -132,10 +137,50 @@ local currency."
                 type="Featured Project"
               />
             </div>
-            <div className="col-span-6">Project-2</div>
-            <div className="col-span-12">Featured Project</div>
-            <div className="col-span-6">Project-3</div>
-            <div className="col-span-6">Project-4</div>
+            <div className="col-span-6">Project
+            <Project
+                title=" Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-12">Featured Project
+            <Project
+                title=" Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">Project<Project
+                title=" Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              /></div>
+            <div className="col-span-6">Project<Project
+                title=" Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              /></div>
           </div>
         </Layout>
       </main>
