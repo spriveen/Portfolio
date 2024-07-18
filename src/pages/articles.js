ac010ad7b615f4b13b46e3762c271aa9c19d6df7
spriveen <rpeanuka@gmail.com>
2024-07-18 03:35:20 -0700

    update articles.js

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,24 +1,54 @@
-import React from 'react'
-import Head from 'next/head'
-import Layout from '@/components/Layout'
-import AnimatedText from '@/components/AnimatedText'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react';
+import Head from 'next/head';
+import Layout from '@/components/Layout';
+import AnimatedText from '@/components/AnimatedText';
+import Image from 'next/image';
+import Link from 'next/link';
+import articles1 from "../../public/images/articles/pagination component in reactjs.jpg";
+import articles2 from "../../public/images/articles/create loading screen in react js.jpg";
+import { motion } from 'framer-motion';
+import articles3 from "../../public/images/articles/create modal component in react using react portals.png";
 
+ 
+
+const FramerImage = motion(Image);
+
+const Article=({img,date,link,title}) => {
+       return(
+        <li className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center
+        justify-between bg-light text-dark first:mt-0 border border-solid border-dark
+        border-r-4 border-b-4
+        '>
+          <Link href={link} target="_blank">
+          <h2 className="capitalize  text-x1 font-semibold hover:underline">{title}</h2>
+          </Link>
+          <span>{date}</span>
+        </li>
+       )
+}
 
  const FeaturedArticle = ({img,title,time,summary,link})=>{
   return(
-    <li>
+    <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
+     rounded-br-3xl
+     '/> 
       <Link
         href={link}
         target="_blank"
-        className="w-full cursor-pointer overflow-hidden rounded-lg"
+        className="w-full inline-block cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
-      </Link>
+        <FramerImage src={img} alt={title} className="w-full h-auto"
+        whileHover={{scale:1.05}}
+        transtition={{duration:0.2}}
+        
+        />
+         </Link>
       <Link href={link} target='_blank'>
-      <h2 className='capitalize text-2xl font-bold my-2'>{title}</h2>
+      <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline'>{title}</h2>
       </Link>
+      <p className='text-sm mb-2'>{summary}</p>
+      <span className='text-primary font-semibold'>{time}</span>
     </li>
   )
 }
@@ -34,9 +64,69 @@ const articles = () => {
         <Layout className='pt-16'>
             <AnimatedText text="Words Can Change The World!" className='mb-16' />
                 <ul className='grid grid-cols-2 gap-10'>
-                    <li>Featured articles-1</li>
-                    <li>Featured articles-2</li>
+                    <FeaturedArticle
+title="Build A Custom Pagination Component In Reactjs From Scratch"
+summary="Learn how to build a custom pagination component in ReactJS from scratch. 
+Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+time="9 min read"
+link="/"
+img={articles1}
+                     />
+                     <FeaturedArticle
+title="Build A Custom Pagination Component In Reactjs From Scratch"
+summary="Learn how to build a custom pagination component in ReactJS from scratch. 
+Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+time="9 min read"
+link="/"
+img={articles2}
+                     />
                 </ul>
+                <h2 className='font-bold text-4xl w-full text-center my-16 mt-32'>All Articles</h2>
+            <ul>
+               <Article
+               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+               date="March 22 2023"
+               link="/"
+               img={articles3}
+               />
+               <Article
+               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+               date="March 22 2023"
+               link="/"
+               img={articles3}
+               />
+               <Article
+               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+               date="March 22 2023"
+               link="/"
+               img={articles3}
+               />
+               <Article
+               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+               date="March 22 2023"
+               link="/"
+               img={articles3}
+               />
+               <Article
+               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+               date="March 22 2023"
+               link="/"
+               img={articles3}
+               />
+               <Article
+               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+               date="March 22 2023"
+               link="/"
+               img={articles3}
+               />
+               <Article
+               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+               date="March 22 2023"
+               link="/"
+               img={articles3}
+               />
+            </ul>
+        
         </Layout>
       </main>
   
--- src/pages/project.js
+++ src/pages/project.js
@@ -6,6 +6,9 @@ import Link from "next/link";
 import Image from "next/image";
 import { GithubIcon } from "@/components/Icon";
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import { motion } from "framer-motion";
+
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({ type, title, img, link, github, summary }) => {
   return (
@@ -22,7 +25,12 @@ const FeaturedProject = ({ type, title, img, link, github, summary }) => {
         target="_blank"
         className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto" 
+        
+        
+        whileHover={{scale:1.05}}
+        transtition={{duration:0.2}}
+        />
       </Link>
 
       <div className="w-1/2 flex flex-col items-center justify-between pl-6">
@@ -67,7 +75,10 @@ const Project = ({ title, type, img, link, github }) => {
         target="_blank"
         className="w-full cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto"
+        whileHover={{scale:1.05}}
+        transtition={{duration:0.2}}
+        />
       </Link>
 
       <div className="w-full flex flex-col items-center justify-between mt-4">
