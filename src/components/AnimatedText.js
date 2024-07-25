1b2305bbf1dce54268048c7be5d216199e779ea7
spriveen <rpeanuka@gmail.com>
2024-07-25 07:41:09 -0700

    fix the bug project.js file

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,62 +1,53 @@
 import React from 'react'
-import {animate, motion, stagger} from "framer-motion"
+import { motion, stagger } from 'framer-motion'
 
 const quote ={
-  initial:{
-    opacity:1,
-  },
-  animate:{
-    opacity:1,
-    transition:{
-      delay:0.5,
-      staggerChildren: 0.08,
-      
+    initial:{
+        opacity: 0,
+    },
+    animate:{
+        opacity: 1,
+        transition:{
+            delay:0.5,
+            staggerChildren: 0.08,
+        }
     }
-  }
 }
 
-const singleword ={
-  initial:{
-    opacity:0,
-    y:50
-  },
-  animate:{
-    opacity:1,
-    y:0,
-    transitin:{
-      duration:1
+const singleWord ={
+    initial:{
+        opacity: 0,
+        y:50,
+    },
+    animate:{
+        opacity: 1,
+        y:0,
+        transition:{
+            duration:1,
+        }
     }
-  }
 }
-
 
 const AnimatedText = ({text,className=""}) => {
   return (
-    <div className='w-full mx-auto py-2 flex items-center justify-center text-center
-     overflow-hidden 
-     '> 
-       <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl 
-       dark:text-light
-       ${className}`}
-        variants={quote}
-        initial="initial"
-        animate="animate"
-
-        >
-        
+    <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden
+   sm:py-0'>
+      <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl 
+       dark:text-light ${className}`}
+      variants={quote}
+      initial="initial"
+      animate="animate"
+      >
         {
             text.split(" ").map((word,index)=>
-                <motion.span key={word+'-'+ index}className="inline-block"
-                variants={singleword}
+                <motion.span key={word+ '-' +index} className="inline-block"
+                variants={singleWord}
                 >
-                    
-                    
                     {word}&nbsp;
-                </motion.span>
-            )
-        }
-        </motion.h1> 
-        </div>
+                </motion.span>)
+}
+      </motion.h1>
+    </div>
   )
 }
 
--- src/pages/project.js
+++ src/pages/project.js
@@ -1,136 +1,105 @@
-import Layout from "@/components/Layout";
-import React from "react";
-import Head from "next/head";
-import AnimatedText from "@/components/AnimatedText";
-import Link from "next/link";
-import Image from "next/image";
-import { GithubIcon } from "@/components/Icon";
-import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
-import { motion } from "framer-motion";
+
+import { GithubIcon } from '@/components/Icon'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import project1 from '../../public/images/projects/crypto-screener-cover-image.jpg'
+import { motion } from 'framer-motion'
+import AnimatedText from '@/components/AnimatedText'
 
 const FramerImage = motion(Image);
 
-const FeaturedProject = ({ type, title, img, link, github, summary }) => {
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article
-      className="w-full flex items-center justify-between relative rounded-br-2xl
-    rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light
-     "
-    >
-     <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light
-     rounded-br-3xl
-     '/> 
-      <Link
-        href={link}
-        target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+    <article className='w-full flex items-center justify-between relative rounded-br-2xl
+    rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light'>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark
+        rounded-br-3xl dark:bg-light' />
+      <Link href={link} target="_blank"
+        className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
       >
-        <FramerImage src={img} alt={title} className="w-full h-auto" 
+        <FramerImage src={img} alt={title} className='w-full h-auto' 
         whileHover={{scale:1.05}}
-        transtition={{duration:0.2}}
-       priority
-               sizes="(max-width:768px) 100vw
-               (ma px-width:1200px) 50vw
-               50"
-        />
+        transition={{duration: 0.2}}
+        priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px)50vw, 50vw"/>
       </Link>
 
-      <div className="w-1/2 flex flex-col items-center justify-between pl-6">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light">{title}</h2>
+      <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
+        <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
+          <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light'>{title}</h2>
         </Link>
-        <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
-        <div className="mt-2 flex items-center">
-          <Link href={github} target="_blank" className="w-10">
-            {" "}
-            <GithubIcon />
-          </Link>
-          <Link
-            href={link}
-            target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
-            dark:bg-light dark:text-dark
-            "
-          >
-            Visit project
-          </Link>
+        <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
+        <div className='mt-2 flex items-center'>
+          <Link href={github} target="_blank" className='w-10'><GithubIcon /></Link>
+          <Link href={link} target="_blank" className='ml-4 rounded-lg bg-dark text-light
+        p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark'>Visit the Project</Link>
         </div>
       </div>
     </article>
-  );
-};
+  )
+}
 
 const Project = ({ title, type, img, link, github }) => {
   return (
-    <article className="w-full flex flex-col items-center justify-center rounded-2xl
-    border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-x-light
-    ">
-
-<div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
-     rounded-br-3xl dark:bg-light
-     '/> 
-      <Link
-        href={link}
-        target="_blank"
-        className="w-full cursor-pointer overflow-hidden rounded-lg"
+    <article className='w-full flex flex-col items-center justify-center rounded-2xl
+    border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light'>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
+        rounded-br-3xl dark:bg-light'/>
+      <Link href={link} target="_blank"
+        className='w-full cursor-pointer overflow-hidden rounded-lg'
       >
-        <FramerImage src={img} alt={title} className="w-full h-auto"
+        <FramerImage src={img} alt={title} className='w-full h-auto' 
         whileHover={{scale:1.05}}
-        transtition={{duration:0.2}}
-        />
+        transition={{duration: 0.2}}/>
       </Link>
 
-      <div className="w-full flex flex-col items-center justify-between mt-4">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+      <div className='w-full flex flex-col items-start justify-between mt-4'>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
+        <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
+          <h2 className='my-2 w-full text-left text-3xl font-bold'>{title}</h2>
         </Link>
-        
-        <div className="w-full mt-2 flex items-center justify-between">
-        <Link
-            href={link}
-            target="_blank"
-            className="text-lg font-semibold underline"
-          >
-            Visit
-          </Link>
-          <Link href={github} target="_blank" className="w-10">
-            <GithubIcon />{""}
-          </Link>
-          
+
+        <div className='w-full mt-2 flex items-center justify-between'>
+          <Link href={link} target="_blank" className=' text-lg font-semibold underline'>Visit</Link>
+          <Link href={github} target="_blank" className='w-10'><GithubIcon /></Link>
         </div>
       </div>
     </article>
-  );
-};
+  )
+}
 
 const project = () => {
   return (
     <>
       <Head>
-        <title>Riveen Peanuka | projects Page</title>
+        <title>Riveen | Project Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
-        <Layout className="pt-16">
-          <AnimatedText
-            text="Imagination Trumps Knowledge!"
-            className="mb-16"
-          />
+      <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
+        <Layout className='pt-16'>
+          <AnimatedText text="Imagination Trumps Knowledge!"
+            className='mb-16' />
 
-          <div className="grid grid-cols-12 gap-24 gap-y-32">
-            <div className="col-span-12">
+          <div className='grid grid-cols-12 gap-24 gap-y-32'>
+            <div className='col-span-12'>
               <FeaturedProject
-                title=" Crypto Screener Application"
+                title="Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+              It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+              local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className='col-span-6'>
+              <Project
+                title="Crypto Screener Application"
                 img={project1}
                 summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
 It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
@@ -140,9 +109,9 @@ local currency."
                 type="Featured Project"
               />
             </div>
-            <div className="col-span-6">
-            <Project
-                title=" Crypto Screener Application"
+            <div className='col-span-6'>
+              <Project
+                title="Crypto Screener Application"
                 img={project1}
                 summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
 It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
@@ -152,9 +121,10 @@ local currency."
                 type="Featured Project"
               />
             </div>
-            <div className="col-span-6">Project
-            <Project
-                title=" Crypto Screener Application"
+
+            <div className='col-span-12'>
+              <FeaturedProject
+                title="Crypto Screener Application"
                 img={project1}
                 summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
 It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
@@ -164,9 +134,9 @@ local currency."
                 type="Featured Project"
               />
             </div>
-            <div className="col-span-12">Featured Project
-            <Project
-                title=" Crypto Screener Application"
+            <div className='col-span-6'>
+              <Project
+                title="Crypto Screener Application"
                 img={project1}
                 summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
 It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
@@ -176,8 +146,9 @@ local currency."
                 type="Featured Project"
               />
             </div>
-            <div className="col-span-6">Project<Project
-                title=" Crypto Screener Application"
+            <div className='col-span-6'>
+              <Project
+                title="Crypto Screener Application"
                 img={project1}
                 summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
 It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
@@ -185,22 +156,14 @@ local currency."
                 link="/"
                 github="/"
                 type="Featured Project"
-              /></div>
-            <div className="col-span-6">Project<Project
-                title=" Crypto Screener Application"
-                img={project1}
-                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency."
-                link="/"
-                github="/"
-                type="Featured Project"
-              /></div>
+              />
+            </div>
           </div>
         </Layout>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default project;
+export default project
+
