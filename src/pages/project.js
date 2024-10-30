af0b235a4465547e5a2f83d6623822d3a5d8febd
spriveen <rpeanuka@gmail.com>
2024-10-30 02:05:35 -0700

    update project .js

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,4 +1,3 @@
-
 import { GithubIcon } from '@/components/Icon'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
@@ -15,7 +14,6 @@ import project7 from '../../public/images/projects/5.jpg'
 import project8 from '../../public/images/projects/2.jpg'
 import project9 from '../../public/images/projects/7.jpg'
 
-
 import { motion } from 'framer-motion'
 import AnimatedText from '@/components/AnimatedText'
 
@@ -27,8 +25,8 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
     rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light
     lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4'>
 
-<div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark
-        rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]' />
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark
+          rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]' />
       <Link href={link} target="_blank"
         className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'
       >
@@ -48,7 +46,7 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
         <div className='mt-2 flex items-center'>
           <Link href={github} target="_blank" className='w-10'><GithubIcon /></Link>
           <Link href={link} target="_blank" className='ml-4 rounded-lg bg-dark text-light
-        p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base'>Visit the Project</Link>
+          p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base'>Visit the Project</Link>
         </div>
       </div>
     </article>
@@ -60,7 +58,7 @@ const Project = ({ title, type, img, link, github }) => {
     <article className='w-full flex flex-col items-center justify-center rounded-2xl
     border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4'>
       <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
-        rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]'/>
+          rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]'/>
       <Link href={link} target="_blank"
         className='w-full cursor-pointer overflow-hidden rounded-lg'
       >
@@ -97,10 +95,9 @@ const project = () => {
           <AnimatedText text="Creativity Outshines Knowledge!"
             className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl' />
 
-          <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 '>
+          <div className='grid grid-cols-12 gap-28 gap-y-36 xl:gap-x-20 lg:gap-x-10 md:gap-y-28 sm:gap-x-4 '>
             <div className='col-span-12'>
               <FeaturedProject
-
                 title="Gym Nest Application"
                 img={project1}
                 summary="Gym Nest is a dynamic, user-friendly website for fitness enthusiasts
@@ -111,11 +108,9 @@ const project = () => {
                 type="Featured Project"
               />
             </div>
-            </div>
-            <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 '>
+
             <div className='col-span-12'>
               <FeaturedProject
-
                 title="Cab Hub"
                 img={project3}
                 summary="Cab Hub is a modern, intuitive platform built with Next.js, TSX, and Tailwind CSS, connecting riders and drivers effortlessly. 
@@ -125,11 +120,9 @@ const project = () => {
                 type="Featured Project"
               />
             </div>
-           </div>
-           <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 '>
+
             <div className='col-span-12'>
               <FeaturedProject
-
                 title="Burger Shop"
                 img={project2}
                 summary="Burger Shop is a modern responsive website crafted with Next.js, Tailwind CSS, and TypeScript. It features an intuitive interface for browsing the menu
@@ -139,28 +132,25 @@ const project = () => {
                 type="Featured Project"
               />
             </div>
-            </div>
-            <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 '>
             <div className='col-span-12'>
               <FeaturedProject
-
                 title="Fashion Web Site"
                 img={project8}
-                summary="I created a stylish fashion website using HTML, CSS and JavaScript. The site features an elegant design, allowing users to browse the latest trends
+                summary="I created a stylish fashion website using HTML, CSS and JavaScript.
+                 The site features an elegant design, allowing users to browse the latest trends
                  view collections and shop effortlessly for their favorite styles."
                 link="/"
                 github="https://github.com/spriveen/Fashion-Web-Site"
                 type="Featured Project"
               />
             </div>
-            </div>
-            <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 '>
+
             <div className='col-span-12'>
               <FeaturedProject
-
                 title="Online Courses Website"
                 img={project5}
-                summary="I developed an online course website using HTML, CSS, and JavaScript. The platform provides an interactive learning experience
+                summary="I developed an online course website using HTML, CSS, and JavaScript. 
+                The platform provides an interactive learning experience
                  allowing users to browse coursesaccess resources and engage with
                   instructors seamlessly and effectively"
                 link="/"
@@ -168,52 +158,50 @@ const project = () => {
                 type="Featured Project"
               />
             </div>
-            </div>
-            <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 '>
+
             <div className='col-span-12'>
               <FeaturedProject
-
                 title="Online E-Channeling System"
                 img={project6}
-                summary="E-Channeling System is a sophisticated online platform developed using React.js, Node.js, and MongoDB. This system offers a seamless and intuitive user experience for
-                 booking appointments. Users can easily search for healthcare providers, schedule consultations and manage their appointments efficiently. The responsive design ensures accessibility across devices
+                summary="E-Channeling System is a sophisticated online platform developed using React.js, Node.js, and MongoDB. 
+                This system offers a seamless and intuitive user experience for booking appointments. Users can easily search for healthcare providers, schedule consultations and manage their appointments efficiently. The responsive design ensures accessibility across devices
                   while secure data management enhances user privacy. This solution streamlines the healthcare process
-                making it convenient for users to access medical services and connect with professionals effortlessly"
+                making it convenient for users to access medical services and connect with professionals effortlessly."
                 link="/"
                 github="https://github.com/spriveen/Doctor-Appointment"
                 type="Featured Project"
               />
             </div>
-             </div>
-             <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 '>
+
             <div className='col-span-12'>
               <FeaturedProject
-
                 title="Coffee Shop"
                 img={project7}
                 summary="Online Coffee Shop System is an innovative e-commerce platform developed using React.js. 
-                This dynamic website provides a visually appealing and user-friendly interface, allowing customers to effortlessly browse a diverse selection of coffee products, place orders and manage their accounts.
-                 With features like secure payment processing and real-time inventory updates, the system ensures a seamless shopping experience. The responsive design optimizes usability across devices, making it easy for coffee lovers to indulge in their favorite brews anytime, anywhere. This system elevates the online coffee shopping experience connecting customers with premium coffee offerings"
+                This dynamic website provides a visually appealing and user-friendly interface allowing customers to effortlessly browse a diverse selection of coffee products, place orders and manage their accounts.
+                 With features like secure payment processing and real-time inventory updates, the system ensures a seamless shopping experience. The responsive design optimizes usability across devices, making it easy for coffee lovers to indulge in their favorite brews anytime, anywhere. This system elevates the online coffee shopping 
+                 experience connecting customers with premium coffee offerings"
                 link="/"
-                github="https://github.com/spriveen/GYM"
+                github="https://github.com/spriveen/Burger-Landing"
                 type="Featured Project"
               />
             </div>
-           </div>
-           <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 '>
+
             <div className='col-span-12'>
               <FeaturedProject
-
                 title="Digital News"
                 img={project9}
-                summary="Digital News Platform is a modern website built with Next.js, JSX, Tailwind CSS, Bootstrap and MongoDB. It delivers real-time news updates
-                 engaging articles and a responsive design, ensuring a seamless user experience across devices"
+                summary="Digital News Platform is a modern website built with Next.js, JSX, Tailwind CSS, Bootstrap and MongoDB. 
+                It delivers real-time news updatesengaging articles and a responsive design, ensuring a 
+                seamless user experience across devices"
                 link="/"
                 github="https://github.com/spriveen/-Digital-news-app"
                 type="Featured Project"
               />
             </div>
-            </div>
+
+            {/* Add additional FeaturedProject components here if needed */}
+          </div>
         </Layout>
       </main>
     </>
