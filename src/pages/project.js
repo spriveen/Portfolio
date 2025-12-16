1762dd72466272f36658b36aebf1766d95ae2c9b
spriveen <rpeanuka@gmail.com>
2025-12-15 21:32:28 -0800

    updTE project.js

--- src/pages/project.js
+++ src/pages/project.js
@@ -33,6 +33,10 @@ import project26 from '../../public/images/projects/24.jpg'
 import project27 from '../../public/images/projects/25.jpg'
 import project28 from '../../public/images/projects/26.jpg'
 import project29 from '../../public/images/projects/27.jpg'
+import project30 from '../../public/images/projects/28.jpg'
+
+
+
 
 import { motion } from 'framer-motion'
 import AnimatedText from '@/components/AnimatedText'
@@ -438,6 +442,18 @@ const project = () => {
               <FeaturedProject
                 title="Full-stack Car Rental"
                 img={project29}
+                summary="Full-stack Car Rental Website built with the MERN stack, featuring JWT authentication, car listings, booking management, and an admin dashboard. Includes ImageKit media optimization and a scalable, production-ready architecture"
+                link="/"
+                github="https://github.com/spriveen/-Full-Stack-Car-Rental-Booking-App-"
+                type="Featured Project"
+              />
+              </div>
+
+
+              <div className='col-span-12'>
+              <FeaturedProject
+                title="Cricbuzz"
+                img={project30}
                 summary="Full-stack Car Rental Website built with the MERN stack, featuring JWT authentication, car listings, booking management, and an admin dashboard. Includes ImageKit media optimization and a scalable, production-ready architecture"
                 link="/"
                 github="https://github.com/spriveen/-Full-Stack-Car-Rental-Booking-App-"
