b27bb55fd0d70f6b61a2376965d67bfcb6a2fb0b
spriveen <rpeanuka@gmail.com>
2026-01-07 06:51:12 -0800

    update abou.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -88,7 +88,7 @@ I’m passionate about building seamless, efficient, and scalable digital soluti
                 </div>
                 <div className='flex flex-col items-end justify-center xl:items-center'>
                     <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                    <AnimatedNumbers value={4}/>+
+                    <AnimatedNumbers value={1}/>+
                     </span>
                     <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75
                     xl:text-center md:text-lg sm:text-base xs:text-sm'>Years of experience</h2>
